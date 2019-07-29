c146982310f4f3ccb6051603a84336468446c319
Belleve Invis <belleve@typeof.net>
2019-07-29 00:15:04 -0700

    basics of parser

--- src/parser/interface.ts
+++ src/parser/interface.ts
@@ -1,8 +1,28 @@
 import { Box } from "../box";
 import { Param } from "../param";
 
-export type Form = string | CompositeForm;
-export interface CompositeForm extends Array<Form> {}
+export enum TokenType {
+	ID = 1,
+	BRACKET = 2,
+	SYMBOL = 3,
+	TEXT = 4,
+	TT = 5,
+	SPACE = 6
+}
+export type Token = { type: TokenType; c: string };
+
+export type Form = Token | Invoke;
+export interface Invoke {
+	macro: Macro;
+	args: Form[];
+}
+export function Invoke(macro: Macro, ...args: Form[]): Invoke {
+	return { macro, args };
+}
+
+export function isToken(form: Form): form is Token {
+	return (form as any).type;
+}
 
 export interface Macro {
 	readonly arity: number;
@@ -20,6 +40,12 @@ export type MacroRegisterer = (s: Scope) => void;
 
 export interface Builder {
 	readonly param: Param;
-	withScope(scope: Scope): Builder;
 	build(form: Form): Box;
+}
+
+export interface Primitives {
+	empty: Macro;
+	HCombine: Macro;
+	brackEnclosure: Macro;
+	parenEnclosure: Macro;
 }
---
+++ src/parser/lex.ts
@@ -0,0 +1,42 @@
+import { Token, TokenType } from "./interface";
+
+const reToken = /("(?:[^\\\"]|\\.)*")|(`(?:[^`]|``)*`)|([\[\(\{<]:|:[\]\}\)>])|(:?[a-zA-Z0-9\u0080-\uffff]+:?|\.\d+)|([\[\]\(\)\{\}])|([\.\,\;]|[\/<>?:'|\\\-_+=~!@#$%^&*]+)/g;
+function walk(
+	r: RegExp,
+	s: string,
+	fMatch: (...matches: string[]) => void,
+	fGap: (gap: string) => void
+) {
+	let l = r.lastIndex;
+	r.lastIndex = 0;
+	fMatch = fMatch || function() {};
+	fGap = fGap || function() {};
+	let match,
+		last = 0;
+	while ((match = r.exec(s))) {
+		if (last < match.index) fGap(s.slice(last, match.index));
+		fMatch(...match);
+		last = r.lastIndex;
+	}
+	if (last < s.length) fGap(s.slice(last));
+	r.lastIndex = l;
+	return s;
+}
+export function lex(s: string) {
+	let q: Token[] = [];
+	walk(
+		reToken,
+		s,
+		function(m, text, tt, rs, id, b, sy) {
+			if (text) q.push({ type: TokenType.TEXT, c: text.replace(/\\"/g, '"') });
+			if (tt) q.push({ type: TokenType.TT, c: tt });
+			if (rs) q.push({ type: TokenType.SYMBOL, c: rs });
+			if (id) q.push({ type: TokenType.ID, c: id });
+			if (b) q.push({ type: TokenType.BRACKET, c: b });
+			if (sy) q.push({ type: TokenType.SYMBOL, c: sy });
+			return "";
+		},
+		function(space) {}
+	);
+	return q;
+}
